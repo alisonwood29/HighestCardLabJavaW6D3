500d6c70a39853e23da0dd5b8b7dfc0b9d943b77
Alison Wood <alisonwood29@gmail.com>
2018-01-10 14:03:06 +0000

    added test and method to populate deck

--- src/main/java/Deck.java
+++ src/main/java/Deck.java
@@ -1,2 +1,25 @@
+import java.util.ArrayList;
+
 public class Deck {
+
+    private ArrayList<Card> cards;
+
+    public Deck(){
+        this.cards = new ArrayList<>();
+    }
+
+    public int cardCount() {
+        return this.cards.size();
+    }
+
+    public void populate() {
+        Suit[] suitValues = Suit.values();
+        Rank[] rankValues = Rank.values();
+        for(Suit suit: suitValues){
+            for(Rank rank: rankValues){
+                Card card = new Card(suit, rank);
+                this.cards.add(card);
+            }
+        }
+    }
 }
